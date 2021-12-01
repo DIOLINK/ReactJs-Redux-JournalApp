da7c038d80f6f032a8590322d7ad461d59bdaea7
Roger BC <diolink.4103@gmail.com>
2021-12-01 20:35:45 -0300

    Journal, Sass is implemented

--- src/components/Journal/JournalPage.js
+++ src/components/Journal/JournalPage.js
@@ -1,7 +1,12 @@
+import { Sidebar } from './Sidebar';
+
 export const JournalPage = () => {
   return (
-    <div>
-      <h1>Journal Page</h1>
+    <div className="journal__main-content">
+      <Sidebar />
+      <main>
+        <h1>hola main</h1>
+      </main>
     </div>
   );
 };
