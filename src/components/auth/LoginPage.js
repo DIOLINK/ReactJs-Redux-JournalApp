f9371584c9823ec76c7b8eb6376d560633faf290
Roger BC <diolink.4103@gmail.com>
2021-12-02 18:34:55 -0300

    config Redux

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,27 +1,47 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { login } from '../../actions/auth';
+import useForm from '../../hooks/useForm';
 export const LoginPage = () => {
+  const dispatch = useDispatch();
+  const [formValues, handleInputChange, reset] = useForm({
+    email: 'roger@gmail.com',
+    password: '123456',
+  });
+  const { email, password } = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(1234, 'Rocher'));
+    reset();
+  };
+
   return (
     <>
       <h3 className="auth__title">Login...</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           className="auth__input"
           type="text"
           name="email"
           placeholder="Email..."
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
-          type="text"
+          type="password"
           name="password"
           placeholder="Password..."
+          value={password}
+          onChange={handleInputChange}
         />
+        <button type="submit" className="btn btn-primary btn-block">
+          Login
+        </button>
       </form>
-      <button type="submit" className="btn btn-primary btn-block">
-        Login
-      </button>
 
       <div className="auth__social-networks">
         <p>Login with social networks</p>
